f077c7b16be03209edecd8f59e19d4edfd245806
Stanislav Koshchienko <superken126@gmail.com>
2022-07-18 13:28:45 +0200

    3ed

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -11,7 +11,7 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        //Just something plz
+        //Just something plzs
         return "Hello from RESTEasy Reactive";
     }
 }
