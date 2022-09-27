03890e0b1633d8c16c4a69b5dbe6fdef66986949
Stanislav Koshchienko <superken126@gmail.com>
2022-09-27 15:31:29 +0200

    In test there is a fail

--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -15,7 +15,7 @@ public class GreetingResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello from RESTEasy Reactivу FAIL"));
+             .body(is("Hello from RESTEasy Reactivу FAIL ФФФФФФ"));
     }
 
 }
